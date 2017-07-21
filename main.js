fe9773ae1e5be6c52669e0e8092c01977b4f53f1
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2017-07-21 15:16:44 -0600

    on rebuild can enter text

--- main.js
+++ main.js
@@ -1,29 +1,7 @@
-
-angular.module('roicalculatorApp', [])
-  .controller('OneTimeRevenueController', function() {
+angular.module('roiCalculatorApp', [])
+  .controller ('OneTimeRevenueController', function(){
     var oneTimeRev = this;
-    oneTimeRev.todos = [
+    oneTimeRev.oneTimes = [
 
-      ];
-
-    oneTimeRev.addTodo = function() {
-      oneTimeRev.todos.push({text:oneTimeRev.todoText});
-      v.todoText = '';
-    };
-
-    oneTimeRev.remaining = function() {
-      var count = 0;
-      angular.forEach(oneTimeRev.todos, function(todo) {
-        count += todo.done ? 0 : 1;
-      });
-      return count;
-    };
-
-    oneTimeRev.archive = function() {
-      var oldTodos = oneTimeRev.todos;
-      oneTimeRev.todos = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) oneTimeRev.todos.push(todo);
-      });
-    };
-  });
+    ];
+})
