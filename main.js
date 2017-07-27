7172fae78fac6ae77ebbf90146434df4bf170b2f
jasonkarlgoldfarb@gmail.com <jasonkarlgoldfarb@gmail.com>
2017-07-26 20:34:16 -0600

    added logic for %

--- main.js
+++ main.js
@@ -13,7 +13,13 @@
       return sum;
     };
 
-  });
+  })
+  // Calculate percentage
+.filter('percentage', ['$filter', function ($filter) {
+      return function (input, decimals) {
+        return $filter('number')(input * 100, decimals) + '%';
+      };
+    }])
 
   ROICalculator.controller('ROIController', ['$scope', function($scope){
     $scope.revenueEnters = [];
